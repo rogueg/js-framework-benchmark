31be8f8d273e8157f6dfb3ed3c6d3ca67a7350e8
Grant Marvin <rogueg@gmail.com>
2017-01-27 17:17:21 -0800

    Revert "improve build"
    This reverts commit bce306374f638b700075f400c55d755db0b751fb.

--- build.js
+++ build.js
@@ -2,11 +2,8 @@ var _ = require('lodash');
 var exec = require('child_process').execSync;
 var fs = require('fs');
 
-var excludedDirectories = ['css', 'dist','node_modules','webdriver-java'];
-
 _.each(fs.readdirSync('.'), function(name) {
-	if(fs.statSync(name).isDirectory() && name[0] !== '.' && excludedDirectories.indexOf(name)==-1 && !skip) {
-		console.log("Executing npm run build-prod in "+name);
+	if(fs.statSync(name).isDirectory() && name[0] !== '.' && name !== 'css' && name !== 'dist'  && name !== 'node_modules' && name !== 'webdriver-java') {
 		exec('npm run build-prod', {
 			cwd: name,
 			stdio: 'inherit'
--- install.js
+++ install.js
@@ -3,18 +3,15 @@ var exec = require('child_process').execSync;
 var fs = require('fs');
 var commandExists = require('command-exists');
 
-var installCommand = 'npm install';
-
-var excludedDirectories = ['css', 'dist','node_modules','webdriver-java'];
+var instalCommand = 'npm install';
 
 commandExists('yarn', function(err, commandExists) {
 
-	installCommand = commandExists ? 'yarn' : 'npm install';
+	instalCommand = commandExists ? 'yarn' : 'npm install';
 
 	_.each(fs.readdirSync('.'), function(name) {
-		if(fs.statSync(name).isDirectory() && name[0] !== '.' && excludedDirectories.indexOf(name)==-1) {
-			console.log("Executing "+installCommand+" in "+name);
-			exec(installCommand, {
+		if(fs.statSync(name).isDirectory() && name[0] !== '.' && ['css', 'dist','node_modules'].indexOf(name)==-1) {
+			exec(instalCommand, {
 				cwd: name,
 				stdio: 'inherit'
 			});
